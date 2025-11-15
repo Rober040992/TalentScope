13f62825e78e13aa44ae1dc98faec7375c4c9fba
RoberGF <rgfrasta@gmail.com>
2025-11-15 11:28:12 +0100

    update: trycatch app.js

--- src/app.js
+++ src/app.js
@@ -5,9 +5,17 @@ import { connectDB } from "./config/mongooseConfig.js";
 
 // conectamos a mongo, obtenemos datos de Arbeitnow crea servidor Graphql con apollo (typeDefs y resolvers) y arranca el localhost
 export async function startServer() {
-  await connectDB();
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error, "error connection on DB");
+  }
 
   const server = new ApolloServer({ typeDefs, resolvers });
-  const { url } = await server.listen({ port: process.env.PORT || 4000 });
-  console.log(`🚀 TalentScope GraphQL API running at ${url}`);
+  try {
+    const { url } = await server.listen({ port: process.env.PORT || 4000 });
+    console.log(`🚀 TalentScope GraphQL API running at ${url}`);
+  } catch (error) {
+    console.log(error, "error listening the server");
+  }
 }

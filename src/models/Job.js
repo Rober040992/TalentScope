a8434fc506c784f8e3bc51e63ead9026a4f89063
RoberGF <rgfrasta@gmail.com>
2025-11-16 10:29:28 +0100

    Update: Jobs model

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,12 +1,37 @@
 import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema({
-  title: String,
-  company_name: String,
-  location: String,
-  url: String,
-  tags: [String],
-  created_at: Date,
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  company_name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  url: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // indice único por url , Dedupicado por url
